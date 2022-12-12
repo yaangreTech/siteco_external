d63c87098402792e16989b68aae8eea813289beb
ManuYaro <yaroemmanuelbekassoe@gmail.com>
2022-12-12 20:47:46 +0000

    Update frame1.dart

--- lib/frame1.dart
+++ lib/frame1.dart
@@ -14,7 +14,9 @@ import 'functions/global_data.dart';
 GlobalData data = GlobalData();
 //the id of the project
 var project_id;
-
+//configuration mode
+enum radioValues { auto, custom}
+var config_mode;
 //frame 1
 class Frame1 extends StatefulWidget {
   const Frame1({Key? key}) : super(key: key);
@@ -25,6 +27,7 @@ class Frame1 extends StatefulWidget {
 class _Frame1State extends State<Frame1> {
   bool? chkBox1 = true;
   bool? chkBox2 = false;
+
 
   @override
   void initState() {
@@ -41,11 +44,16 @@ class _Frame1State extends State<Frame1> {
       project_id = data.formValues["id"];
     }
 
+    //sets the default value for the configuration style
+    setState(() {
+      config_mode = radioValues.custom;
+    });
+
   }
 
   @override
   Widget build(BuildContext context) {
-
+    var isDisabled = config_mode == radioValues.auto ? true: false;
     return Padding(
       padding: const EdgeInsets.fromLTRB(150, 0, 150, 0),
       child: Column(
@@ -99,16 +107,22 @@ class _Frame1State extends State<Frame1> {
                 children: [
                   Row(
                     children: [
-                      Checkbox(
-                        activeColor: red,
-                        //checkColor: red,
-                          value: chkBox1,
-                          onChanged: (bool? value){
-                            setState(() {
-                              chkBox1 = value;
-                            });
-                          }
-                      ),
+                      Radio(activeColor: red, value: radioValues.custom, groupValue: config_mode, onChanged: (_)
+                      {
+                        setState(() {
+                          config_mode = _;
+                        });
+                      }),
+                      // Checkbox(
+                      //   activeColor: red,
+                      //   //checkColor: red,
+                      //     value: chkBox1,
+                      //     onChanged: (bool? value){
+                      //       setState(() {
+                      //         chkBox1 = value;
+                      //       });
+                      //     }
+                      // ),
                       SizedBox(width: 20,),
                       Text("Directly select components", style: TextStyle(fontSize: 22, color: grey))
                     ],
@@ -116,31 +130,43 @@ class _Frame1State extends State<Frame1> {
                   SizedBox(height: 30,),
                   Row(
                     children: [
-                      Checkbox(
-                        activeColor: red,
-                        //checkColor: red,
-                          value: chkBox2,
-                          onChanged: (bool? value){
-                            setState(() {
-                              chkBox2 = value;
-                            });
-                          }
-                      ),
+                      Radio( activeColor: red, value: radioValues.auto, groupValue: config_mode, onChanged: (_)
+                      {
+                        setState(() {
+                          config_mode = _;
+                        });
+                      }),
+                      // Checkbox(
+                      //   activeColor: red,
+                      //   //checkColor: red,
+                      //     value: chkBox2,
+                      //     onChanged: (bool? value){
+                      //       setState(() {
+                      //         chkBox2 = value;
+                      //       });
+                      //     }
+                      // ),
                       SizedBox(width: 20,),
                       Text("Start configuration based on room size (auto-proposed configuration)", style: TextStyle(fontSize: 22, color: grey),)
                     ],
                   ),
                   SizedBox(height: 50,),
                   Container(
-                    decoration: BoxDecoration(border: Border.all(color: grey, width: 1), color: Colors.white),
-                    child: Column(
-                      children: [
-                        RoomConfigItem(label: "Length (m)", sliderValue: 30, max: 60, min: 1),
-                        RoomConfigItem(label: "Width (m)", sliderValue: 30, max: 60, min: 1),
-                        RoomConfigItem(label: "Height (m)", sliderValue: 7, max: 14, min: 1),
-                        RoomConfigItem(label: "Mounting height (m)", sliderValue: 2, max: 14, min: 1),
-                        RoomConfigItem(label: "Desired illuminance (lux)", sliderValue: 300, max: 500, min: 100),
-                      ],
+                    decoration: BoxDecoration(border: Border.all(color: isDisabled ? Colors.white30: grey, width: 1), color: Colors.white),
+                    child: IgnorePointer(
+                      ignoring: isDisabled,
+                      child: ColorFiltered(
+                        colorFilter: ColorFilter.mode(isDisabled ? Colors.white30: Colors.white, BlendMode.modulate),
+                        child: Column(
+                          children: [
+                            RoomConfigItem(label: "Length (m)", sliderValue: 30, max: 60, min: 1),
+                            RoomConfigItem(label: "Width (m)", sliderValue: 30, max: 60, min: 1),
+                            RoomConfigItem(label: "Height (m)", sliderValue: 7, max: 14, min: 1),
+                            RoomConfigItem(label: "Mounting height (m)", sliderValue: 2, max: 14, min: 1),
+                            RoomConfigItem(label: "Desired illuminance (lux)", sliderValue: 300, max: 500, min: 100),
+                          ],
+                        ),
+                      ),
                     ),
                   )
                 ],
